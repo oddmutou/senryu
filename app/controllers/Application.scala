d83079a712c066eae9e4c4e8e3010874b59e7fd4
Nao Muto <n@o625.com>
2016-08-31 16:29:40 +0900

    kari

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,13 +9,58 @@ import ExecutionContext.Implicits.global
 import twitter4j._
 import twitter4j.auth._
 import scala.collection.JavaConverters._
+import org.atilika.kuromoji.Tokenizer
 
 object Application extends Controller {
   
   def index = Action {
     twitterinfo("test")
-    Ok(views.html.index("Your new application is ready."))
+    val userName = "Girls_Sense"
+    val gsRetweets = getTwitter(userName)
+    tweetAnalysis(gsRetweets)
+    Ok(views.html.index("test"))
   }
+
+
+
+  def getTwitter(userName: String): ResponseList[Status] = {
+    val cb = new twitter4j.conf.ConfigurationBuilder
+    // GSのAPIキー
+    cb.setOAuthConsumerKey("e7yWV3wglOOwZVSH74paJvrfl")//キーは自分で取得してください
+      .setOAuthConsumerSecret("oGy5pKh2Ay2jAaouei8wh6p8lDd3hJYYhd0BTa0sb2LZLRO40F")//キーは自分で取得してください
+      .setOAuthAccessToken("127681532-4y8sY9ecGBXPwcgZ7pearohHr3EfZwB5mNgiqoHA")//キーは自分で取得してください
+      .setOAuthAccessTokenSecret("pd7xLhGdqQOohsflGDirAGRiYlBhNjPo8qrIL1p6hk36S")//キーは自分で取得してください
+    
+    val twitterFactory = new TwitterFactory(cb.build)
+    val tt = twitterFactory.getInstance
+
+    val timeLine = tt.timelines
+    timeLine.getRetweetsOfMe
+  }
+
+  def tweetAnalysis(gsRetweets: ResponseList[Status]) = {
+
+    val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
+
+    val tokens = for (rl <- gsRetweets) yield tokenizer.tokenize(rl.getText)
+
+    val sorted = tokens.flatten.filter(x => x.getPartOfSpeech.startsWith("名詞")).groupBy(x => x.getBaseForm).values.toList.sortWith(_.length>_.length)
+
+    sorted.foreach { x =>
+      x match {
+        case x if (x(0).getBaseForm() == null) =>
+        case x => println("count: " +x.length+" "+x(0).getBaseForm())
+      }
+    }
+
+  }
+
+
+
+
+
+
+
 
   def twitterinfo(sw : String) = {
     //認証
@@ -36,7 +81,7 @@ object Application extends Controller {
         + st.getText.replace("\n", "")
         + " tweet at " + st.getCreatedAt.formatted("%tY/%<tm/%<td %<tH:%<tM:%<tS")
         + " URL:https://twitter.com/" + st.getUser.getScreenName + "/status/" + st.getId)
-
+/*
     //自分のタイムラインに表示されている内容を表示
     val sts = getTweet(tt, 0, Nil, 2)
     println("mytimeline表示件数:" + sts.length + "件")
@@ -56,6 +101,7 @@ object Application extends Controller {
         + st.getText.replace("\n", "")
         + " tweet at " + st.getCreatedAt.formatted("%tY/%<tm/%<td %<tH:%<tM:%<tS")
         + " URL:https://twitter.com/" + st.getUser.getScreenName + "/status/" + st.getId)
+*/
   }
 
   def getTweet(tt : twitter4j.Twitter, ni : Long, sts : List[twitter4j.Status], pf : Int, sw : String = "", ln : Int = 100) : List[twitter4j.Status] = {
