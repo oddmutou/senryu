4887110c0fd7b442efdbae8d817e198bb8428879
Nao Muto <n@o625.com>
2016-09-01 10:39:53 +0900

    get message

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,29 +1,41 @@
 package controllers
 import play.api._
+import play.api.cache._
 import play.api.mvc._
 import play.api.libs.ws
 import play.api.libs.ws.WS
+import play.api.Play.current
 import scala.concurrent._
 import scala.concurrent.duration.Duration
 import ExecutionContext.Implicits.global
 import twitter4j._
 import twitter4j.auth._
+import twitter4j.conf._
 import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
 import org.atilika.kuromoji.Tokenizer
 
 object Application extends Controller {
   
   def index = Action {
-    twitterinfo("test")
-    val userName = "Girls_Sense"
-    val gsRetweets = getTwitter(userName)
-    tweetAnalysis(gsRetweets)
-    Ok(views.html.index("test"))
+    val getTwitter : Option[Twitter] = Cache.getAs[Twitter]("twitter_obj")
+    getTwitter match {
+      case Some(twitter) => {
+        val gsRetweets = getTimeline(twitter)
+        tweetAnalysis(gsRetweets)
+        Ok(views.html.index("success!"))
+      }
+      case _ => {
+        Ok(views.html.login("Login"))
+      }
+    }
+    
+
   }
 
 
 
-  def getTwitter(userName: String): ResponseList[Status] = {
+  def getTimeline(getTwitter : Twitter): ResponseList[twitter4j.Status] = {
     val cb = new twitter4j.conf.ConfigurationBuilder
     // GSのAPIキー
     cb.setOAuthConsumerKey("e7yWV3wglOOwZVSH74paJvrfl")//キーは自分で取得してください
@@ -32,21 +44,25 @@ object Application extends Controller {
       .setOAuthAccessTokenSecret("pd7xLhGdqQOohsflGDirAGRiYlBhNjPo8qrIL1p6hk36S")//キーは自分で取得してください
     
     val twitterFactory = new TwitterFactory(cb.build)
-    val tt = twitterFactory.getInstance
+    val tt = getTwitter
 
     val timeLine = tt.timelines
-    timeLine.getRetweetsOfMe
+    timeLine.getUserTimeline
   }
 
-  def tweetAnalysis(gsRetweets: ResponseList[Status]) = {
+  def tweetAnalysis(gsRetweets: ResponseList[twitter4j.Status]) = {
 
     val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
-
-    val tokens = for (rl <- gsRetweets) yield tokenizer.tokenize(rl.getText)
+    
+    val tokens = for (rl <- gsRetweets) yield {
+        println(rl.getText)
+        tokenizer.tokenize(rl.getText)
+    }
 
     val sorted = tokens.flatten.filter(x => x.getPartOfSpeech.startsWith("名詞")).groupBy(x => x.getBaseForm).values.toList.sortWith(_.length>_.length)
 
     sorted.foreach { x =>
+      println(x)
       x match {
         case x if (x(0).getBaseForm() == null) =>
         case x => println("count: " +x.length+" "+x(0).getBaseForm())
@@ -59,81 +75,21 @@ object Application extends Controller {
 
 
 
+  
 
 
 
-  def twitterinfo(sw : String) = {
-    //認証
-    val cb = new twitter4j.conf.ConfigurationBuilder
-    cb.setOAuthConsumerKey("e7yWV3wglOOwZVSH74paJvrfl")//キーは自分で取得してください
-      .setOAuthConsumerSecret("oGy5pKh2Ay2jAaouei8wh6p8lDd3hJYYhd0BTa0sb2LZLRO40F")//キーは自分で取得してください
-      .setOAuthAccessToken("127681532-4y8sY9ecGBXPwcgZ7pearohHr3EfZwB5mNgiqoHA")//キーは自分で取得してください
-      .setOAuthAccessTokenSecret("pd7xLhGdqQOohsflGDirAGRiYlBhNjPo8qrIL1p6hk36S")//キーは自分で取得してください
-    val tf = new TwitterFactory(cb.build)
-    val tt = tf.getInstance
 
-    //自分のツイートした内容を表示
-    val mts = getTweet(tt, 0, Nil, 1)
-    println("mytweet表示件数:" + mts.length + "件")
-    for (st <- mts)
-      println("@" + st.getUser.getScreenName + "/"
-        + st.getUser.getName + ":"
-        + st.getText.replace("\n", "")
-        + " tweet at " + st.getCreatedAt.formatted("%tY/%<tm/%<td %<tH:%<tM:%<tS")
-        + " URL:https://twitter.com/" + st.getUser.getScreenName + "/status/" + st.getId)
-/*
-    //自分のタイムラインに表示されている内容を表示
-    val sts = getTweet(tt, 0, Nil, 2)
-    println("mytimeline表示件数:" + sts.length + "件")
-    for (st <- sts)
-      println("@" + st.getUser.getScreenName + "/"
-        + st.getUser.getName + ":"
-        + st.getText.replace("\n", "")
-        + " tweet at " + st.getCreatedAt.formatted("%tY/%<tm/%<td %<tH:%<tM:%<tS")
-        + " URL:https://twitter.com/" + st.getUser.getScreenName + "/status/" + st.getId)
 
-    //検索文字を指定して検索した結果を表示
-    val twt = getTweet(tt, 0, Nil, 3, sw)
-    println("検索結果:" + twt.length + "件")
-    for (st <- twt)
-      println("@" + st.getUser.getScreenName + "/"
-        + st.getUser.getName + ":"
-        + st.getText.replace("\n", "")
-        + " tweet at " + st.getCreatedAt.formatted("%tY/%<tm/%<td %<tH:%<tM:%<tS")
-        + " URL:https://twitter.com/" + st.getUser.getScreenName + "/status/" + st.getId)
-*/
-  }
 
-  def getTweet(tt : twitter4j.Twitter, ni : Long, sts : List[twitter4j.Status], pf : Int, sw : String = "", ln : Int = 100) : List[twitter4j.Status] = {
-    val pg = new Paging(1, 100)
-    val query = new Query
-    if (ni != 0) pg.setMaxId(ni)
-    try {
-      val mts = if (pf == 1) tt.getUserTimeline(pg)
-      else if (pf == 2) tt.getHomeTimeline(pg)
-      else {
-        query.setQuery(sw)
-        query.setMaxId(ni)
-        val rs = tt.search(query)
-        rs.getTweets
-      }
-      mts.size match {
-        case i if i == 0 || sts.length + i > ln => sts
-        case _ => getTweet(tt, mts.get(mts.size - 1).getId - 1, sts ::: mts.asScala.toList, pf, sw, ln)
-      }
-    } catch {
-      case e : TwitterException => {
-        if (e.getStatusCode == 400 || e.getStatusCode == 429) sts
-        else {
-            e.printStackTrace
-            Nil
-        }
-      }
-      case e : Exception => {
-          e.printStackTrace
-          Nil
-      }
-    }
-  }
+
+
+
+
+
+
+
+
+
 
 }
---
+++ app/controllers/TwitterController.scala
@@ -0,0 +1,123 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.Play.current
+
+import play.api.cache._
+
+import twitter4j._
+import twitter4j.auth._
+
+/**
+ * ツイッターログインコントローラ
+ */
+object TwitterController extends Controller {
+
+  /**
+   * Twitterへログインする
+   */
+  def twitterLogin = Action { implicit request =>
+    val consumerKey = "e7yWV3wglOOwZVSH74paJvrfl"
+    val consumerSecret = "oGy5pKh2Ay2jAaouei8wh6p8lDd3hJYYhd0BTa0sb2LZLRO40F"
+    // Twitterオブジェクトの初期化
+    val twitter: Twitter = (new TwitterFactory()).getInstance()
+    println("http://vps2.o625.com/twitterOAuthCallback")
+    // RequestTokenの取得
+
+    twitter.setOAuthConsumer(consumerKey, consumerSecret)
+    
+    val requestToken: RequestToken = twitter.getOAuthRequestToken("http://vps2.o625.com/twitterOAuthCallback")
+
+    // TwitterとRequestTokenのオブジェクトをCacheに格納(2分有効)
+    Cache.set("twitter", twitter, 120)
+    Cache.set("requestToken", requestToken, 120)
+
+    // Twitterのログイン画面にリダイレクト
+    Redirect(requestToken.getAuthorizationURL())
+  }
+
+
+  /**
+   * TwitterからのCallBack処理
+   */
+  def twitterOAuthCallback = Action { implicit request =>
+    // 承認可否を精査（deniedがあったら承認キャンセル）
+    request.queryString.get("denied") match {
+
+      // Twitterのアプリケーション承認キャンセル時
+      case Some(denied) => Redirect(routes.TwitterController.twitterLogout)
+
+      // Twitterのアプリケーション承認時
+      case _ => {
+
+        // TwitterのオブジェクトをCacheから取得
+        val getTwitter     : Option[Twitter] = Cache.getAs[Twitter]("twitter")
+
+        // 取得できない場合はトップ画面へ
+        getTwitter match {
+          case Some(twitter) => {
+
+            // RequestTokenのオブジェクトをCacheから取得
+            val getRequestToken: Option[RequestToken] = Cache.getAs[RequestToken]("requestToken")
+
+            // 取得できない場合はトップ画面へ
+            getRequestToken match {
+              case Some(requestToken) => {
+
+                // AuthTokenを取得する
+                var authToken   : String = request.queryString.get("oauth_token").get.head
+                var authVerifier: String = request.queryString.get("oauth_verifier").get.head
+
+                // AccessTokenを取得する
+                // val accessToken: AccessToken = twitter.getOAuthAccessToken(requestToken, authVerifier)
+                var token = twitter.getOAuthAccessToken(requestToken, authVerifier)
+                println(token)
+                // AccessTokenを取得する場合
+                // val accessToken: AccessToken = twitter.getOAuthAccessToken(requestToken, authVerifier)
+                // val accessTokenSt: String = accessToken.getToken
+                // val accessTokenSecretSt: String = accessToken.getTokenSecret
+
+                // Twitterオブジェクトの認証
+                twitter.verifyCredentials()
+
+                // TwitterのUserオブジェクトを取得
+                var user: User = twitter.showUser(twitter.getId())
+
+                // Cacheに設定(3日間有効)
+                Cache.set("twitter_user", user, 4320)
+                Cache.set("twitter_obj", twitter, 4320)
+
+                // Cacheから削除
+                Cache.remove("twitter")
+                Cache.remove("requestToken")
+
+              }
+              case _ =>
+            }
+          }
+          case _ =>
+        }
+
+        // リターン
+        Redirect(routes.Application.index)
+      }
+    }
+  }
+
+  /** 
+   * ログアウト処理
+   */
+  def twitterLogout = Action { implicit request =>
+
+    // Cacheから削除
+    Cache.remove("twitter_user")
+
+    // リターン
+    Redirect(routes.Application.index)
+  }
+}
+
+
+
+
